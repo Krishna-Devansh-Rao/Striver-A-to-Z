9a1c7484c9b2cbe03f444e32def93e42d38f05aa
KRISHNA DEVANSH RAO <100765720+Krishna-Devansh-Rao@users.noreply.github.com>
2026-04-13 21:13:58 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+// Boiler Plate
+// int main() {
+//     // Write your code here
+//     return 0;
+// }
+
+// Data Types
+// int main() {
+//     int age = 25;
+//     float height = 5.9;
+//     char grade = 'A';
+//     bool isStudent = true;
+//     string name = "Rahul";
+//     return 0;
+// }
+
+// Input Output
+// int main() {
+//     int age;
+//     // Output: Ask user a question
+//     cout <<"Enter your age: ";
+//     // Input: Read what user types and store in 'age'
+//     cin >> age;
+//     // Output: Show the result
+//     cout << "Your age is: " << age;
+//     return 0;
+// }
+
+
