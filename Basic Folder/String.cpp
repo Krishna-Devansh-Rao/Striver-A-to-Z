55cfc40ea35ab401873c4ecce2dde671726f8046
KRISHNA DEVANSH RAO <100765720+Krishna-Devansh-Rao@users.noreply.github.com>
2026-04-14 08:24:02 +0530

    Delete Basic Folder directory

--- Basic Folder/String.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-    string str = "Krishna";
-    for(int i =0;i < str[i];i++){
-        cout << str[i];
-    }
-}
--- Basic Folder/boilercode.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-// Boiler Plate
-int main() {
-    // Write your code here
-    return 0;
-}
